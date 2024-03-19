fa576a4d8b8449d5c338eb6a5fa186cdfc75175a
Udita1510 <pandyaudita40@gmail.com>
2024-03-19 19:15:27 +0530

    changed

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
+
+
+
 # Create your models here.
 class People(models.Model):
+    
     PERSON_TYPE_CHOICES = [
         ('electrician', 'Electrician'),
         ('mishtry', 'Mishtry'),
@@ -36,3 +41,42 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.customer_name
+      
+# # # # # # # # # # # # # # # ## # # # # # # # # # # # ## # # # # # # # # # # ##
+
+class udita_People(models.Model):
+    Person_type_chioce = [
+        ('worker','Worker'),
+        ('teacher','Teacher'),
+        ('employee','Employee'),
+    ]
+    
+    peoples_name = models.CharField(max_length = 255, blank = True, null = True)
+    mobile_no = models.CharField(max_length = 255, blank = True, null = True)
+
+    people_type = models.CharField(max_length=255, blank= True, null = True, choices=Person_type_chioce)
+    
+    def __str__(self):
+        return self.peoples_name
+    
+class udita_Building(models.Model):
+    building_name = models.CharField(max_length=255, blank = True, null = True)
+    company_name = models.CharField(max_length=255, blank = True, null = True)
+    office_no = models.CharField(max_length = 255, blank = True, null = True)
+    location = models.CharField(max_length = 255, blank = True, null = True)
+    address = models.CharField(max_length= 255, blank = True, null = True)
+    
+    def __str__(self):
+        return self.building_name
+    
+class udita_Customer(models.Model):
+    customer_name = models.CharField(max_length=255, blank = True, null = True)
+    email = models.EmailField(blank = True, null = True)
+    phone_no = models.CharField(max_length=15, blank = True, null = True)  
+    customer_address = models.CharField(max_length=255, blank = True, null = True)  
+    
+
+    def __str__(self):
+        return self.customer_name
+
+
